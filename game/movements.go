9f93775f34fbeeea224f09757d67921458986dd6
Tommaso Visconti <tommaso.visconti@gmail.com>
2018-06-13 21:59:56 +0200

    refactor movements

--- game/movements.go
+++ game/movements.go
@@ -1,39 +1,51 @@
 package game
 
+import "github.com/tommyblue/sokoban"
+
+type direction struct {
+	dX int
+	dY int
+}
+
+var right = direction{dX: 0, dY: 1}
+var left = direction{dX: 0, dY: -1}
+var up = direction{dX: -1, dY: 0}
+var down = direction{dX: 1, dY: 0}
+
 func (ge *GameEngine) MoveLeft() {
-	i := ge.Game.CurrentLevel.CurrentPlayerPosition.PositionI
-	j := ge.Game.CurrentLevel.CurrentPlayerPosition.PositionJ
-	if ge.canMoveThere(i, j-1) {
-		ge.Game.CurrentLevel.CurrentPlayerPosition.PositionJ = j - 1
-	}
+	ge.moveIfPossible(left)
 }
 
 func (ge *GameEngine) MoveRight() {
-	i := ge.Game.CurrentLevel.CurrentPlayerPosition.PositionI
-	j := ge.Game.CurrentLevel.CurrentPlayerPosition.PositionJ
-	if ge.canMoveThere(i, j+1) {
-		ge.Game.CurrentLevel.CurrentPlayerPosition.PositionJ = j + 1
-	}
+	ge.moveIfPossible(right)
 }
 
 func (ge *GameEngine) MoveUp() {
-	i := ge.Game.CurrentLevel.CurrentPlayerPosition.PositionI
-	j := ge.Game.CurrentLevel.CurrentPlayerPosition.PositionJ
-	if ge.canMoveThere(i-1, j) {
-		ge.Game.CurrentLevel.CurrentPlayerPosition.PositionI = i - 1
-	}
+	ge.moveIfPossible(up)
 }
 
 func (ge *GameEngine) MoveDown() {
-	i := ge.Game.CurrentLevel.CurrentPlayerPosition.PositionI
-	j := ge.Game.CurrentLevel.CurrentPlayerPosition.PositionJ
-	if ge.canMoveThere(i+1, j) {
-		ge.Game.CurrentLevel.CurrentPlayerPosition.PositionI = i + 1
-	}
+	ge.moveIfPossible(down)
 }
 
-func (ge *GameEngine) canMoveThere(i, j int) bool {
-	tileID := ge.Game.CurrentLevel.Tiles[i][j]
+func (ge *GameEngine) canMoveThere(d direction) bool {
+	newI := ge.Game.CurrentLevel.CurrentPlayerPosition.PositionI + d.dX
+	newJ := ge.Game.CurrentLevel.CurrentPlayerPosition.PositionJ + d.dY
+	tileID := ge.Game.CurrentLevel.Tiles[newI][newJ]
 
 	return tileID != "#"
+}
+
+func (ge *GameEngine) move(d direction) {
+	currI := ge.Game.CurrentLevel.CurrentPlayerPosition.PositionI
+	currJ := ge.Game.CurrentLevel.CurrentPlayerPosition.PositionJ
+	ge.Game.CurrentLevel.CurrentPlayerPosition.PositionI = currI + d.dX
+	ge.Game.CurrentLevel.CurrentPlayerPosition.PositionJ = currJ + d.dY
+}
+
+func (ge *GameEngine) moveIfPossible(d direction) *sokoban.PlayerPosition {
+	if ge.canMoveThere(d) {
+		ge.move(d)
+	}
+	return ge.Game.CurrentLevel.CurrentPlayerPosition
 }
